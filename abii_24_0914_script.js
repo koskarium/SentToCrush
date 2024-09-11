4456ad29b6a2658d8ef60d05f9ea2706c0516eb7
koskarium <koskar@csu.fullerton.edu>
2024-09-11 00:19:05 -0700

    Update abii_24_0914_script.js

--- abii_24_0914_script.js
+++ abii_24_0914_script.js
@@ -5,7 +5,7 @@ const gif = document.querySelector(".gif");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Let's Meet Here";
+  question.innerHTML = "Let us meet here";
   gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
 });
 

e7a922b1f6f67221739d84df0ca69280c7f51eca
koskarium <koskar@csu.fullerton.edu>
2024-09-11 00:50:22 -0700

    Update abii_24_0914_script.js

--- abii_24_0914_script.js
+++ abii_24_0914_script.js
@@ -3,8 +3,28 @@ const noBtn = document.querySelector(".no-btn");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 
-// Function to move the No button to a random position
-const moveNoButton = () => {
+// Change text and gif when the Yes button is clicked
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Let us meet here";
+  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
+    
+  // Hide the Yes and No buttons by setting their display to 'none'
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+});
+
+// Make the No button move to a random location within the parent container on hover
+noBtn.addEventListener("mouseover", () => {
+  moveNoBtnRandomly();
+});
+
+// Move the No button to a random location within the parent container on click
+noBtn.addEventListener("click", () => {
+  moveNoBtnRandomly();
+});
+
+// Function to move the No button to a random location within its parent container
+function moveNoBtnRandomly() {
   const wrapper = document.querySelector(".wrapper");
   const wrapperRect = wrapper.getBoundingClientRect();
   const noBtnRect = noBtn.getBoundingClientRect();
@@ -16,22 +36,7 @@ const moveNoButton = () => {
   const randomX = Math.floor(Math.random() * maxX);
   const randomY = Math.floor(Math.random() * maxY);
 
+  noBtn.style.position = "absolute";
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
-};
-
-// Change text and gif when the Yes button is clicked
-yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Let us meet here";
-  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
-    
-  // Hide the Yes and No buttons by setting their display to 'none'
-  yesBtn.style.display = "none";
-  noBtn.style.display = "none";
-});
-
-// Make the No button move randomly on hover
-noBtn.addEventListener("mouseover", moveNoButton);
-
-// Move the No button when clicked
-noBtn.addEventListener("click", moveNoButton);
+}

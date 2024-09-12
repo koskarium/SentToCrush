c38d8e7418324cd042e83f6ffaabbba2a4551c44
koskarium <koskar@csu.fullerton.edu>
2024-09-12 10:22:52 -0700

    Update abii_24_0914_script.js

--- abii_24_0914_script.js
+++ abii_24_0914_script.js
@@ -58,8 +58,7 @@ setInitialNoBtnPosition();
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Let us meet here";
-  gifFrame.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
-  
+  gifFrame.src = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGR3czZtZTVtZGZsN2lqZmsxbHE4NTFncTZmMzFqeWhuam01N2dzZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/8Pj8VpvnuyNNK/giphy.gif"
   // Hide the Yes and No buttons
   yesBtn.style.display = "none";
   noBtn.style.display = "none";

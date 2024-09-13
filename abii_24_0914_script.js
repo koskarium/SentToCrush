714a13128dc9574dfb501f9fffff77acac824d59
Oskar <koskarnegron@gmail.com>
2024-09-13 14:03:11 -0700

    Update abii_24_0914_script.js

--- abii_24_0914_script.js
+++ abii_24_0914_script.js
@@ -54,12 +54,12 @@ setInitialNoBtnPosition();
 // Change text, iframe, and add a hyperlink when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Let us meet here";
-  iframe.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3302.378338600552!2d-118.36570968778906!3d34.136661513154436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2be46c8139ab7%3A0x8900c6cfa93b252e!2sThe%20Baked%20Potato!5e0!3m2!1sen!2sus!4v1726166371706!5m2!1sen!2sus";
+  iframe.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3305.0491061622406!2d-118.26494162567225!3d34.06825541688703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c7067649fab5%3A0x2b00aa39537f030!2s1642!5e0!3m2!1sen!2sus!4v1726261217356!5m2!1sen!2sus";
   
   // Create and insert a hyperlink
   const link = document.createElement("a");
-  link.href = "https://maps.app.goo.gl/Y9Boixas97DNHtqw8";
-  link.textContent = "3787 Cahuenga Blvd, Studio City, CA 91604";
+  link.href = "https://maps.app.goo.gl/LC1hjRibcWdePddF6";
+  link.textContent = "1642 W Temple St, Los Angeles, CA 90026";
   link.target = "_blank"; // Open in a new tab
   link.style.display = "block"; // Ensure it displays below the iframe
 
